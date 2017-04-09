49b9fbc3f55e490e4f5f9ad57e5e040d5904d0d1
Kuro <kuro_q@live.com>
2017-04-09 18:21:11 +0800

    -> add Helloworld.ConsoleApp.Tests

---
+++ Helloworld/Helloworld.ConsoleApp.Tests/ConsoleRunner_Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Helloworld.ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld.ConsoleApp.Tests
+{
+    [TestClass()]
+    public class ConsoleRunner_Tests
+    {
+        [TestMethod()]
+        public void Run_Test()
+        {
+
+        }
+    }
+}
--- Helloworld/Helloworld.ConsoleApp/Program.cs
+++ Helloworld/Helloworld.ConsoleApp/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Helloworld.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    public class ConsoleRunner
+    {
+        public void Run()
+        {
+
         }
     }
 }
